f02a71f45d69bbf5cf88eed1a6632aaa80fb530a
mdshepard <mdshepard8@gmail.com>
2018-06-25 16:57:01 -0400

    forgot cors. Added it.

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const { Client } = require('pg');
 const express = require('express');
+const cores = require('cors')
 
-// create an express application
+// create an express application and enable cors
 const app = express();
 app.use(express.json());
+app.use(cors());
 // create a postgresql client
+
 const client = new Client({
     database: 'foodApp'
 });
