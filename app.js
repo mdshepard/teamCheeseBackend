6d9fdbd8950afa9401131b0295b658741c4e4d8f
mdshepard <mdshepard8@gmail.com>
2018-06-25 17:15:24 -0400

    Please work.

--- app.js
+++ app.js
@@ -6,11 +6,18 @@ const cors = require('cors');
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 // create a postgresql client
 
 const client = new Client({
     database: 'foodApp'
 });
+
+
 
 app.get("/data", (req, res) => {
     res.end("cheesedingle")
