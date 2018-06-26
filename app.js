7025665ab61938a58ee010585f3d46a69ba467c5
Jake Berg <jacobvictorberg@gmail.com>
2018-06-26 09:58:38 -0400

    added process.env.port for the app.listen

--- app.js
+++ app.js
@@ -23,6 +23,6 @@ app.get("/data", (req, res) => {
     res.end("cheesedingle")
 });
 
-app.listen(3000, () => {
+app.listen( process.env.port || 3000, () => {
     console.log("this is working")
 })
